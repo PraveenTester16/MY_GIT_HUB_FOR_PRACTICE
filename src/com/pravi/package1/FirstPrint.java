10cb8e9cddb0510ef99fb8f56fc37efc1d62ce33
PraveenTester16 <praveen.siran16@gmail.com>
2025-04-08 10:45:15 +0530

    code changes

--- src/com/pravi/package1/FirstPrint.java
+++ src/com/pravi/package1/FirstPrint.java
@@ -4,5 +4,7 @@ public class FirstPrint {
 
     public static void main(String[] args) {
         System.out.println("Please go to Master branch");
+
+        System.out.println("I made a change to this code");
     }
 }
